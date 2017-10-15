3f50b149a2bab0856ac82392bd990e0397c773dc
moshiur <rmoshiur705@gmail.com>
2017-10-15 09:54:35 +0600

    variable

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello To javascript Developer")
+
+//-------------------Variable basic---------------------
+
+var age = 26;
+console.log(age);
+var fname = 'moshiur';
+var lname = 'rahman';
+
+console.log(fname + " "+lname);
+
+var name = prompt("what is your name? ?");
+
+alert(name);
+
+//---------------------------Condition--------------------------
+

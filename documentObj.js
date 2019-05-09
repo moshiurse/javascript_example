9c712cf5cdb4d93451c764b59d7492b27ff01b9b
Moshiur Rahman <rmoshiur705@gmail.com>
2019-05-09 22:04:54 +0600

    Window & Document Objects

---
+++ documentObj.js
@@ -0,0 +1,11 @@
+document.title;// Title of current document
+document.lastModified;// Date on w hich document was last modified
+document.URL;// Returns string containing URL of current document
+document.domain;// Returns domain of current document
+
+document.write();// Writes text to document (see restrictions on p226)
+document.getElementByld();// Returns element, if there is an element with the value of the id attribute that matches;
+document.querySe1ectorA11();// Returns list of elements that match a CSS selector, which is specified as a parameter each element on the page.
+
+document.createElement();// Creates new element (see p222)
+document.createTextNode();// Creates new text node (see p222)
---
+++ windowObj.js
@@ -0,0 +1,16 @@
+window.innerHeight;//Height of window
+window.innerWidth;//Width of window
+window.pageXOffset;//Distance document has been scrolled horizontally
+window.pageYOffset;//Distance document has been scrolled vertically (in pixels)
+window.screenX;//X-coordinate of pointer, relative to top left corner of screen
+window.screenY;//Y-coordinate of pointer, relative to top left corner of screen
+window.location;//Current URL of window object
+window.document;//Reference to document object, which is used to represent the current page contained in window
+window.history;//Reference to history object for browser window or tab, which contains details of the pages that have been viewed in that window or tab
+window.history.length;//Number of items in hi story object for browser window or tab
+window.screen;// Reference to screen object
+window.screen.width;// Accesses screen object and finds value of its width property (in pixels)
+window.screen.height;//A ccesses screen object and finds value of its height property (in pixels)
+window.alert();
+window.open();
+window.print();
